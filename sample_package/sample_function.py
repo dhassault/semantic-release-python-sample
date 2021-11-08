8724b6dd76668b19dcfd4929293a4384f1afebb8
Yann <yann.leguilly@gmail.com>
2021-11-08 21:29:59 +0900

    feat: implement the multiplication

--- sample_package/sample_function.py
+++ sample_package/sample_function.py
@@ -12,3 +12,19 @@ def my_sum(x_val: int, y_val: int) -> int:
         y_val, int
     ), "Input parameters should be integers."
     return x_val + y_val
+
+
+def my_mul(x_val: int, y_val: int) -> int:
+    """Multiply 2 integers.
+
+    Args:
+        x_val (int): integer to multiply.
+        y_val (int): integer to multiply.
+
+    Returns:
+        int: result of the multiplication.
+    """
+    assert isinstance(x_val, int) and isinstance(
+        y_val, int
+    ), "Input parameters should be integers."
+    return x_val * y_val
